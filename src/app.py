ff2732d104fe1a3ddd3be28697912369f6291d22
Syllaweg <sylvere68@hotmail.fr>
2020-10-24 23:58:26 +0200

    add fct charge_model

--- src/app.py
+++ src/app.py
@@ -13,13 +13,18 @@ from typing import List, Dict, Union, ByteString, Any # Syntaxe pour Typer ses v
 import sys  # module system
 
 
-
-# pip install pyyaml
+"""
+# need pip install pyyaml
 with open("src/config.yaml", 'r') as stream:
     try:
         APP_CONFIG = yaml.full_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
+"""
 
 
-app = Flask(__name__) # instance de Flask, (si name == main alors app run)
+app = Flask(__name__) # instance de Flask, (si name == main -> app run)
+
+def charge_model(path='.', model_name="model.pkl"):
+    learn = load_learner(path, fname=model_name)
+    return learn
