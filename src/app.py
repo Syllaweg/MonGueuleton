76e05c0b961e33e8aaa5a4d8b2c5bbb2fd9c6f86
Syllaweg <sylvere68@hotmail.fr>
2020-10-24 18:16:09 +0200

    add import comment

--- src/app.py
+++ src/app.py
@@ -2,8 +2,6 @@ import fastai
 from fastai.vision import *
 import torch
 
-
-
 from flask import Flask
 import requests
 
@@ -11,5 +9,5 @@ import yaml
 import json
 
 from io import BytesIO  # Stock data sous forme d'octets, comme les variables
-from typing import List, Dict, Union, ByteString, Any # marque les types
-import sys 
+from typing import List, Dict, Union, ByteString, Any # Syntaxe pour Typer ses varb
+import sys  # module system
