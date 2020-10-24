f7c0050fd088082e68be6b763c5455c1cb9f4595
Syllaweg <sylvere68@hotmail.fr>
2020-10-24 18:37:02 +0200

    add read yaml file

--- src/app.py
+++ src/app.py
@@ -11,3 +11,12 @@ import json
 from io import BytesIO  # Stock data sous forme d'octets, comme les variables
 from typing import List, Dict, Union, ByteString, Any # Syntaxe pour Typer ses varb
 import sys  # module system
+
+
+
+# pip install pyyaml
+with open("src/config.yaml", 'r') as stream:
+    try:
+        APP_CONFIG = yaml.full_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
